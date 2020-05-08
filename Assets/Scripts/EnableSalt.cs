c154328bd60aefdeda7a7338703a12ba90364f41
Paulo <paulo.zti@gmail.com>
2020-05-07 22:26:33 -0300

    Adicionado elementos requisitados para deixar o jogo mais interessante

---
+++ Assets/Scripts/EnableSalt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSalt : MonoBehaviour
+{
+    public GameObject saltToEnable;
+
+    public bool shouldDisable = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (saltToEnable != null && other.CompareTag("Player"))
+        {
+            if(shouldDisable)
+                saltToEnable.SetActive(false);
+            else
+                saltToEnable.SetActive(true);
+        }
+    }
+}
